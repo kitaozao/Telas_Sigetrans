fa79059ea632a982783222186393d702f3d209aa
Cristhian <kito_and@hotmail.com>
2016-09-23 16:03:47 -0300

    Tela de obitos pronta

--- nodetestl/public/javascripts/obitos.js
+++ nodetestl/public/javascripts/obitos.js
@@ -2,5 +2,34 @@ var app = angular.module('obitosctrl', []);
 
 app.controller('obitos', function($scope, $http){
 
-    $scope.obitos ={};
+
+    $scope.geral={};
+    $scope.fatores={};
+    $scope.condutas={};
+    $scope.usuario={};
+    $scope.outros={};
+    $scope.orgaos={};
+    $scope.causa={};
+    $scope.obitos ={"infoGeral":$scope.geral,
+                    "fatoresDeRisco":$scope.fatores,
+                    "condutasDeRisco":$scope.condutas,
+                    "usuario":$scope.usuario,
+                    "outros":$scope.outros,
+                    "orgaosResponsaveis":$scope.orgaos,
+                    "causa":$scope.causa};
+
+    $scope.submit = function(){
+
+
+        console.log("enviando formulario:");
+        console.log($scope.obitos);
+
+        $http.post('127.0.0.1:9000', $scope.obitos).
+        success(function(data){
+            console.log(":)")
+        }).error(function(data){
+            console.log(":(")
+        });
+
+    };
 });
