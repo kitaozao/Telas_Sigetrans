9881a733456151e2780f1a4d752e66cc5a563829
Cristhian <kito_and@hotmail.com>
2016-09-26 21:34:22 -0300

    commit dia 26/09 reparos parciais no JSON

--- nodetestl/public/javascripts/obitos.js
+++ nodetestl/public/javascripts/obitos.js
@@ -8,15 +8,27 @@ app.controller('obitos', function($scope, $http){
     $scope.condutas={};
     $scope.usuario={};
     $scope.outros={};
-    $scope.orgaos={};
+    $scope.orgaos=[{name:"PM", id:"pm"},{name:"PRF", id:"prf"},{name:"PRE", id:"pre"},{name:"CETTRANS", id:"cettrans"},{name:"Outro", id:"outro"}];
+    $scope.orgaosSelecionados=[];
     $scope.causa={};
     $scope.obitos ={"infoGeral":$scope.geral,
                     "fatoresDeRisco":$scope.fatores,
                     "condutasDeRisco":$scope.condutas,
                     "usuario":$scope.usuario,
                     "outros":$scope.outros,
-                    "orgaosResponsaveis":$scope.orgaos,
+                    "orgaosResponsaveis":$scope.orgaosSelecionados,
                     "causa":$scope.causa};
+
+    $scope.toogleSelection = function toogleSelection(nomeOrgao){
+        var idx = $scope.orgaosSelecionados.indexOf(nomeOrgao);
+
+        if(idx > -1){
+            $scope.orgaosSelecionados.splice(idx, 1);
+        }else{
+            $scope.orgaosSelecionados.push(nomeOrgao);
+        }
+
+    };
 
     $scope.submit = function(){
 
